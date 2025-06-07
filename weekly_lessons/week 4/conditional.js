5c2fa32ae623992341b9c1ddd8d890fbfa382d26
kokwana-phaks <phaksdevops@gmail.com>
2025-06-07 22:16:55 +0200

    week 4 conditional: Password

---
+++ weekly_lessons/week 4/conditional.js
@@ -0,0 +1,8 @@
+var userInputPassword = "007";
+var actualPassword = "007";
+
+if(userInputPassword == actualPassword) {
+    document.getElementById("output").innerHTML=("Correct Password.  Loggin!....");
+}else {
+    document.getElementById("output").innerHTML=("Incorrect Password. Try again.");
+}
