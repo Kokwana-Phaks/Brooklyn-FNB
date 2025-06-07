fdd67cec6417c777c08ce71fc269b77aa75c93ff
kokwana-phaks <phaksdevops@gmail.com>
2025-06-07 23:43:30 +0200

    week 4 function parameters

---
+++ weekly_lessons/week 4/function2Parameters.js
@@ -0,0 +1,13 @@
+function changeText() {
+    if (document.getElementById("heading").innerHTML =='JavaScript') {
+        document.getElementById("heading").innerHTML = 'Is awesome';
+    } else {
+        document.getElementById("heading").innerHTML = 'JavaScript';
+    }
+}
+
+addNumbers(10, 7);
+
+function addNumbers(num1, num2) {
+    document.getElementById("addition").innerHTML = num1 + num2;
+}
