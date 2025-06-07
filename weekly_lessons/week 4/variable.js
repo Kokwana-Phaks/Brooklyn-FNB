df9933f043b886ac5b472038a5411a1d990267da
kokwana-phaks <phaksdevops@gmail.com>
2025-06-07 21:06:22 +0200

    week 4 variable part 1

---
+++ weekly_lessons/week 4/variable.js
@@ -0,0 +1,10 @@
+var text1;
+var text2;
+var tex3;
+
+text1 = "Hello everyone on the planet"
+text2 = "I love learning JS!"
+tex3 = "... JS is awesome"
+
+document.getElementById("heading1").innerHTML=(text1);
+document.getElementById("paragraph").innerHTML=(text2 + tex3);
