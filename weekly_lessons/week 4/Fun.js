0f5d3cb5684d256ad153f61f69243c93921d9090
kokwana-phaks <phaksdevops@gmail.com>
2025-06-07 23:12:30 +0200

    week 4 function continued on L6

---
+++ weekly_lessons/week 4/Fun.js
@@ -0,0 +1,7 @@
+function changeText() {
+    if (document.getElementById("heading").innerHTML =='JavaScript') {
+        document.getElementById("heading").innerHTML = 'Is awesome';
+    } else {
+        document.getElementById("heading").innerHTML = 'JavaScript';
+    }
+}
