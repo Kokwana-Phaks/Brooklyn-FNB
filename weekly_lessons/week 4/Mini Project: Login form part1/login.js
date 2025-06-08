cb6baec66fd29eaf0abd862e35671388ed9c6e89
kokwana-phaks <phaksdevops@gmail.com>
2025-06-08 14:54:20 +0200

    mini project: login form part2

--- weekly_lessons/week 4/Mini Project: Login form part1/login.js
+++ weekly_lessons/week 4/Mini Project: Login form part1/login.js
@@ -1,4 +1,17 @@
 function verifyUser() {
     var username = document.getElementById("usernameInput").value;
     var password = document.getElementById("passwordInput").value;
+
+    checkUserCreds(username, password);
+}
+
+function checkUserCreds(username, password) {
+    var systemUsername = "Bond";
+    var systemPassword = "007";
+
+    if(username == systemUsername && password === systemPassword) {
+        document.getElementById("message").innerHTML = "Correct. Logging you in...";
+    }else{
+        document.getElementById("message").innerHTML = "Username or Password is incorrect";
+    }
 }
