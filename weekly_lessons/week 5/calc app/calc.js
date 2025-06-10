ef8df8c139e634e78b12fc666bb6c56c5571f047
kokwana-phaks <phaksdevops@gmail.com>
2025-06-11 01:22:43 +0200

    Add Operator button event handler

--- weekly_lessons/week 5/calc app/calc.js
+++ weekly_lessons/week 5/calc app/calc.js
@@ -1,4 +1,10 @@
+//boolen variable, determines if the next thing the user types should be on the new line.
+
 var newLine = true;
+var value1;
+var currentOperator;
+
+// event handler for when any digit button is pressed.
 
 function digitBtnPressed(button) {
 
@@ -11,7 +17,18 @@ function digitBtnPressed(button) {
     }
 }
 
+// Event handler for when the AC button is pressed
+
 function btnACPressed() {
     document.getElementById("inputBox").value = 0;
     newLine = true;
+}
+
+// operator button event handler
+
+function operatorBtnPressed(operator) {
+    currentOperator = operator;
+    value1 = parseInt(document.getElementById("inputBox").value);
+    newLine = true;
+    
 }
