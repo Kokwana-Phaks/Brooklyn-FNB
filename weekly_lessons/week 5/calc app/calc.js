a13a7f9c936194b5c3c234bc353f9b03bad7834b
kokwana-phaks <phaksdevops@gmail.com>
2025-06-11 00:42:48 +0200

    ad AC button event handler

--- weekly_lessons/week 5/calc app/calc.js
+++ weekly_lessons/week 5/calc app/calc.js
@@ -4,9 +4,14 @@ function digitBtnPressed(button) {
 
     if (newLine == true) {
         document.getElementById("inputBox").value = button;
-        newLine = false;
+        newLine = false;       
     } else {
             var currentValue = document.getElementById("inputBox").value;
             document.getElementById("inputBox").value = currentValue + button;
     }
+}
+
+function btnACPressed() {
+    document.getElementById("inputBox").value = 0;
+    newLine = true;
 }
