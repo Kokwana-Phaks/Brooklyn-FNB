bfb6ab5a0846fa697823b85e390239859fcb1453
kokwana-phaks <phaksdevops@gmail.com>
2025-07-04 01:46:16 +0200

    add operators.py for basic numeric operations

---
+++ operators.py
@@ -0,0 +1,18 @@
+#Operators
+
+#Addition(+)
+#subtraction(-)
+#Multiplication(*)
+#Division(/)
+#Modulus(%)
+#Exponent(**)
+
+x = 10
+y = 2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
