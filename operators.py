716aa5a6f69341cbeef3f88faa46a2d11089233e
kokwana-phaks <phaksdevops@gmail.com>
2025-07-04 01:53:15 +0200

    add operators2.py for string concatenation and multplication of strings

--- operators.py
+++ operators.py
@@ -4,15 +4,20 @@
 #subtraction(-)
 #Multiplication(*)
 #Division(/)
-#Modulus(%)
+#Modulus(%) remainder after you divid
 #Exponent(**)
 
 x = 10
 y = 2
+
+# x = x + 2
+# x += 2
+
+# x -= 2
 
 print(x+y)
 print(x-y)
 print(x*y)
 print(x/y)
 print(x%y)
-print(x**y)
+print(x**y) 
