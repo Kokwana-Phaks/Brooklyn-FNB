7ec495406d913b712d4943a508c883a7a4359b93
kokwana-phaks <phaksdevops@gmail.com>
2025-07-04 02:13:27 +0200

    add control_statement.py for conditional or if else statemen

---
+++ control_statements.py
@@ -0,0 +1,21 @@
+# Control statements
+
+num =  -5
+
+if num > 0 :
+    print("This number is positive")
+else:
+    print("This number is either zero or negative")
+    
+# more than two condititon
+
+num2 = 0
+
+if num2 > 0:
+    print("This number is positive")
+elif num2 == 0:
+    print("This number is zero")
+else:
+    print("This number is negative")
+    
+    
--- operators2.py
+++ operators2.py
@@ -4,4 +4,4 @@ str1 = 'Hello'
 str2 = 'World'
 
 print(str1 + " " + str2)
-print(str1 * 3) 
+print(str1 * 3)  
