110cfa2bd2bf9746fef9b382caab6153c1c02bab
kokwana-phaks <phaksdevops@gmail.com>
2025-07-04 03:10:37 +0200

    add loops.py for while and fofor loops

---
+++ weekly_lessons/Week 7/loops.py
@@ -0,0 +1,33 @@
+#loop
+
+#excute block of code repeatedly or for automate repetive tasts
+#for loop iterate over a sequence of list, tuple, strings etc
+
+'''  for item in sequence:
+      #code to run  print(item)
+'''
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+    
+
+numbers = [1,2,3,4,5,6,7]
+
+for number in numbers:
+    print(numbers)
+    
+for i in range(5): # 0 to 4
+    print(i)
+
+
+#while loop repeat as long as condition is true
+
+count = 1 
+
+while count <= 5:
+    print(count)
+    count += 1 #increasements the count by 1
+
+
