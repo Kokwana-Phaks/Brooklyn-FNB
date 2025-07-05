483433945b20c3e4c2aae65f7b7d9cdb4c9ab994
kokwana-phaks <phaksdevops@gmail.com>
2025-07-05 15:17:36 +0200

    add list2.py to append, inser, remove and sort data

---
+++ weekly_lessons/Week 8/list2.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "banana", "cherry"]
+
+fruits.append("kiwi") #to add to list
+print(fruits)
+
+fruits.insert(1, "orangae") # to add at specific index
+print(fruits)
+
+fruits.remove("kiwi")  #remove the first of kiwi
+print(fruits)
+
+fruits.sort() # Sort in accdeding order
+print(fruits)
+
+fruits.sort(reverse= True) # to sort in desending order
+print(fruits)
+
