da14bc1af8f83db4199f55d6bafa1487f791f1f2
kokwana-phaks <phaksdevops@gmail.com>
2025-07-05 15:03:27 +0200

    add list.py to hold a collection of item string, int and other list

---
+++ weekly_lessons/Week 8/list.py
@@ -0,0 +1,13 @@
+#Built in data structure that can hold a collection of item string, int and list
+# orderded, changeable and allow duplicates
+
+fruits = ["apple", "banana", "cherry"]
+
+print( fruits[0])
+
+
+#Are mutable/ changeable
+
+fruits[1]= "blueberry"
+
+print(fruits)
