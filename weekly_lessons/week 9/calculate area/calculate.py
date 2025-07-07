6f1e5ccfaef55ed55f784b493ffdffbf99022d62
kokwana-phaks <phaksdevops@gmail.com>
2025-07-07 04:11:56 +0200

    App to calculate area with imported modules

---
+++ weekly_lessons/week 9/calculate area/calculate.py
@@ -0,0 +1,11 @@
+#function to calculate the area
+
+def area(length, width):
+    return length * width
+
+#function to calculate parameter
+
+def perimeter(length, width):
+    return 2 * (length, width)
+
+
