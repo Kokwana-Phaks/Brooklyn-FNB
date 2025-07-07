feca7efba5c28acfd4dc81e4e843a052be17851f
kokwana-phaks <phaksdevops@gmail.com>
2025-07-07 05:12:29 +0200

    add exception handler with if else exception that run only no exception ran

---
+++ weekly_lessons/week 9/exception_handler.py
@@ -0,0 +1,14 @@
+#Exception handling makes your code more robust and user-friendly by gracefully handling errors rather than crashing.
+#the else block will run only if no exception ran
+
+try:
+    print(x)
+except NameError:
+    print("variable x is not defined")
+else:
+    print("Everything went wrong")
+    
+x = 7 
+
+if x < 0:
+    raise Exception ("Sorry, no numbers below zero")
