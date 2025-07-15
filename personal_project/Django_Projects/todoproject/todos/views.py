3c8f67cff189437234d58dd8129c4af0a9700c2c
kokwana-phaks <phaksdevops@gmail.com>
2025-07-16 00:49:09 +0200

    create urls in urls.py

--- personal_project/Django_Projects/todoproject/todos/views.py
+++ personal_project/Django_Projects/todoproject/todos/views.py
@@ -3,6 +3,21 @@ from .models import Task
 
 # Create your views here.
 
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todos/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        Task.objects.create(title=title)
+        return redirect('task_list')
+    return render(request, 'todos/add_task.html')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
 
 
 
