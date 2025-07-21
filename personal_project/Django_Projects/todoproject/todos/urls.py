10a98ba090583a0185c0e0cf538b1c80fe80552f
kokwana-phaks <phaksdevops@gmail.com>
2025-07-22 01:28:24 +0200

    fix the bugs

--- personal_project/Django_Projects/todoproject/todos/urls.py
+++ personal_project/Django_Projects/todoproject/todos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .import views
 
+app_name = 'todos'
+
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.add_task, name='add_task'),
-    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task')
 ]
--- personal_project/Django_Projects/todoproject/todos/views.py
+++ personal_project/Django_Projects/todoproject/todos/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'todos/task_list.html', {'tasks': tasks})
-
+    
 def add_task(request):
     if request.method == 'POST':
         title = request.POST.get('title')
@@ -17,7 +19,11 @@ def add_task(request):
 def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
-    return redirect('task_list')
+    return redirect('task_list') 
+   
+    
+def task_list(request):
+    return HttpResponse("Basic test") 
 
 
 
