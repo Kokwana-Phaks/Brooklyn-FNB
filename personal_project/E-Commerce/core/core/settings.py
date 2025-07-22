9227d69b7b800b6935765855f1b24037ef635801
kokwana-phaks <phaksdevops@gmail.com>
2025-07-22 20:08:20 +0200

    fix the structure with accounts, store and core at the same level

--- personal_project/E-Commerce/core/core/settings.py
+++ personal_project/E-Commerce/core/core/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
     'accounts',
-    
-    
+    'store',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
