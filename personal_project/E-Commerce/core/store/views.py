9a4363e26b9dc37f4486da8dc60cbad342a2181f
kokwana-phaks <phaksdevops@gmail.com>
2025-07-22 02:23:30 +0200

    implement product list view

--- personal_project/E-Commerce/core/store/views.py
+++ personal_project/E-Commerce/core/store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.filter(available=True)
+    return render(request, 'store/product/list.html', {'products': products})
+
