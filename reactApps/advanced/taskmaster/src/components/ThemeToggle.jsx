a8263654ee8736756f5f79fbdb4ead1919eabfe5
kokwana-phaks <phaksdevops@gmail.com>
2025-08-10 14:17:43 +0200

    Add: ThemeToggle.jsx

---
+++ reactApps/advanced/taskmaster/src/components/ThemeToggle.jsx
@@ -0,0 +1,16 @@
+import { useTheme } from "../contexts/ThemeContext";
+import {FiSun, FiMoon} from "react-icons/fi";
+
+export default function ThemeToggle() {
+    const {isDark, toggleTheme } = useTheme();
+
+    return (
+        <button
+            onClick={toggleTheme}
+            className="theme-toggle"
+            aria-label={isDark ? "Switch to light mode" : "Switch to dark mode"}
+            >
+                {isDark ? <FiSun /> : <FiMoon />}
+        </button>
+    );
+}
