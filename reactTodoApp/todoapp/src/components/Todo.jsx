c2323520a2f11be9b3892d1c09c1ac0b05ccb61e
kokwana-phaks <phaksdevops@gmail.com>
2025-08-21 21:07:08 +0200

    Add the handleSubmit funtion and e.preventDefault for submitting Todo items

--- reactTodoApp/todoapp/src/components/Todo.jsx
+++ reactTodoApp/todoapp/src/components/Todo.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 
-export default function Todo(){
-    const[todo, setTodo]= useState("")
-    return (
-        <div>
-            <form>
-                <input type="text"
-                value={todo}
-                onChange={(e)=> setTodo(e.target.value)}
-                 />
-                 <button>Add</button>
-            </form>
-        </div>
-    );
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {console.log(todos)}
+    </div>
+  );
 }
