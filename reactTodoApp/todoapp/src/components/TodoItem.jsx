7c7940ece360344ffb653a316b20395112bdfa64
kokwana-phaks <phaksdevops@gmail.com>
2025-08-25 19:51:16 +0200

    Fix the delete button

--- reactTodoApp/todoapp/src/components/TodoItem.jsx
+++ reactTodoApp/todoapp/src/components/TodoItem.jsx
@@ -1,12 +1,16 @@
 import styles from "./todoitem.module.css";
 
-export default function TodoItem({item}) {
-    return (
-        <div className={styles.item}>
-            <div className={styles.itemName}>{item}
-                <span className={styles.deleteButton}>x</span>
-            </div>
-            <hr className={styles.line}/>
-        </div>
-    );
-}  
+export default function TodoItem({ item }) {
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        {item}
+      <span>
+        <button className={styles.deleteButton}>x</button>
+      </span>
+      </div>
+      <hr className={styles.line} />
+    </div>
+  );
+}
+  
