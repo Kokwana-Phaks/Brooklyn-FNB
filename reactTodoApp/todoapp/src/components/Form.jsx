0220de16e4c754c77b2f9b08cc361456d3fcd62a
kokwana-phaks <phaksdevops@gmail.com>
2025-08-26 12:38:11 +0200

    add complete task functionality

--- reactTodoApp/todoapp/src/components/Form.jsx
+++ reactTodoApp/todoapp/src/components/Form.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import styles from "./form.module.css";
 
 export default function Form({ todos, setTodos }) {
-  const [todo, setTodo] = useState("");
+  // const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState({ name: "", done: false });
 
   function handleSubmit(e) {
     e.preventDefault();
     setTodos([...todos, todo]);
-    setTodo("");
+    setTodo({ name: "", done: false });
   }
   return (
     <div>
@@ -17,8 +18,8 @@ export default function Form({ todos, setTodos }) {
             className={styles.modernInput}
             placeholder="Enter Todo Item..."
             type="text"
-            value={todo}
-            onChange={(e) => setTodo(e.target.value)}
+            value={todo.name}
+            onChange={(e) => setTodo({ name: e.target.value, done: false })}
           />
           <button className={styles.modernButton} type="submit">
             Add
